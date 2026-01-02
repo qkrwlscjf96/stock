73e1e3456e5144886edb63a94344d662da9f5835
qkrwlscjf96 <wlscjf9600@gmail.com>
2026-01-02 22:38:06 +0900

    initial commit

---
+++ run_job.bat
@@ -0,0 +1,17 @@
+@echo off
+chcp 65001 > nul
+
+set PYTHON=C:\Users\wlscj\AppData\Local\Programs\Python\Python311\python.exe
+set WORKDIR=C:\Users\wlscj\coding\stock
+set SCRIPT=main.py
+set LOGDIR=C:\Users\wlscj\coding\stock\logs
+
+if not exist "%LOGDIR%" mkdir "%LOGDIR%"
+
+echo === START %DATE% %TIME% === >> "%LOGDIR%\job.log"
+
+cd /d "%WORKDIR%"
+"%PYTHON%" "%SCRIPT%" >> "%LOGDIR%\job.log" 2>&1
+
+echo Exit Code: %ERRORLEVEL% >> "%LOGDIR%\job.log"
+echo === END === >> "%LOGDIR%\job.log"
